89f25bb83902690266d546b394eced16adc02b45
Gabriel Barros <ktbarros07@gmail.com>
2024-12-15 03:05:13 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProdutoComponent } from './views/produto/produto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "produto", component: ProdutoComponent }
+];
---
+++ src/app/components/template/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  imports: [
+    MatTabsModule
+  ],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  selectedTabIndex = 0;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url.includes('home')) {
+          this.selectedTabIndex = 0;
+        } else if (event.url.includes('produto')) {
+          this.selectedTabIndex = 1;
+        }
+      }
+    });
+  }
+
+  onTabChange(index: number): void {
+    if (index == 0) {
+      this.router.navigate(['/home']);
+    } else if (index == 1) {
+      this.router.navigate(['/produto']);
+    }
+  }
+}
